f9fcaae320aa2d8e9f0c42cb501650c4868922e2
robrose455 <robrose455@gmail.com>
2021-05-12 11:19:16 -0500

    Messing around with movement

--- Player.gd
+++ Player.gd
@@ -12,40 +12,31 @@ func _ready():
 func _process(delta):
 	
 	if Input.is_action_just_pressed("A"):
-		
-		velocity.z = -10
+		velocity = transform.basis.z * -10
 		
 	if Input.is_action_just_pressed("S"):
-		
-		velocity.x = -10
+		velocity = transform.basis.x * -10
 		
 	if Input.is_action_just_pressed("W"):
-		
-		velocity.x = 10
+		velocity = transform.basis.x * 10
 		
 	if Input.is_action_just_pressed("D"):
-		
-		velocity.z = 10
+		velocity = transform.basis.z * 10
 		
 		
 	if Input.is_action_just_released("A"):
-		
-		velocity.z = 0
+		velocity = transform.basis.x * 0
 		
 	if Input.is_action_just_released("S"):
-		
-		velocity.x = 0
+		velocity = transform.basis.z * 0
 		
 	if Input.is_action_just_released("W"):
-		
-		velocity.x = 0
+		velocity = transform.basis.z * 0
 		
 	if Input.is_action_just_released("D"):
-		
-		velocity.z = 0
+		velocity = transform.basis.x * 0
 		
 	if Input.is_action_just_pressed("Space") and hasJumped == false:
-		
 		velocity.y = 20
 		hasJumped = true
 		
@@ -59,8 +50,10 @@ func _process(delta):
 	if not coll_info: 
 		
 		if velocity.y >= -weight:
-			
-			print(velocity.y)
 			velocity.y += -0.4
 		
-	pass
+		
+func _unhandled_input(event):
+	
+	if event is InputEventMouseMotion:
+		rotate_x(-event.relative.x / 100)
